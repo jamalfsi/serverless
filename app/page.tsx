bb3fe4d0cdd222f6f456f7f6047c000fbba85845
jamalfsi <jamalfsi@gmail.com>
2024-02-11 10:25:15 +0300

    first commit

---
+++ app/page.tsx
@@ -0,0 +1,17 @@
+import AuthComponent from "@/components/Other/AuthComponent";
+
+export default function Home() {
+  return (
+    <div>
+      <AuthComponent/>
+    </div>
+  //  <AuthComponent/>
+  // <div className="min-h-screen flex justify-center bg-gray-400 items-center">
+  //   <div className="mx-auto flex w-full bg-blue-400 flex-col justify-center space-y-6 sm:w-[350px]">
+  //     <div className="flex flex-col bg-red-400 space-y-2 text-center">
+  //       <h1>Leave Portal</h1>
+  //     </div>
+  //   </div>
+  // </div>
+  );
+}
